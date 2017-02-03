fb208ac58d40dbb05bb5815ed267419c86b669b4
jeeriff <thegreatwhiteboo@Yahoo.com>
2017-02-02 19:19:56 -0500

    Added SimpleArrow file. Implemented SimpleArrow class.  Added SimpleArrow test case to RoleGame.java file.  Added SimpleArrow case to switch statement in WeaponFactory.java file.

--- src/edu/fsu/cs/cen4021/RoleGame.java
+++ src/edu/fsu/cs/cen4021/RoleGame.java
@@ -9,11 +9,17 @@ import edu.fsu.cs.cen4021.armory.WeaponFactory;
 public class RoleGame {
 
     public static void main(String[] args) {
+        //Sword test - implemented by default
         Weapon sword = WeaponFactory.getWeapon("sword");
         System.out.println("Sword has " + sword.hit() + " of damage.");
         int armor = 20;
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with " + armor + " points.");
 
+        //SimpleArrow test
+        Weapon arrow = WeaponFactory.getWeapon("simplearrow");
+        armor = 15;
+        System.out.println("SimpleArrow was able to do " + arrow.hit(armor) +
+                " of damage due to an armor with " + armor + " points.");
         //TODO: Add the remaining weapons here
 
     }
